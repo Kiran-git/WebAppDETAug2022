238fb0034c34174f33b6e561f995ba2312213891
Kiran-git <kirannibgur@gmail.com>
2022-09-09 18:14:58 -0700

    commit all

--- MVCDemo/Program.cs
+++ MVCDemo/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Pizza}/{action=Index}/{id?}");
+    pattern: "{controller=Friend}/{action=Lists}/{id?}");
 
 app.Run();
